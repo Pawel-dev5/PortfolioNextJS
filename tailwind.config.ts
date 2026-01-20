e3dfcd2bf2a272f24a2d4d8eb1ed74295aaf38a4
Pawel-dev5 <p.nowecki@gmail.com>
2026-01-20 10:53:27 +0100

    [FIX] Offer - fix stylind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import tailwindcssAnimate from 'tailwindcss-animate';
 
 export default {
 	darkMode: ['class'],
-	content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+	content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './lib/**/*.{ts,tsx}'],
 	prefix: '',
 	theme: {
 		container: {
